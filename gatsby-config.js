d0bb25bf3add45e0df9de6f0a5ffb89a44c28453
Ian Candy <iancandysemail@gmail.com>
2019-02-21 08:13:56 -0500

    save changes this time :-(

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,18 +20,16 @@ module.exports = {
       },
     },
     {
-<<<<<<< HEAD
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `img`,
         path: `${__dirname}/src/images/`
-      }
-=======
-     resolve: `gatsby-plugin-google-analytics`,
-     options: {
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
        trackingId: "UA-134122184-1",
-     },
->>>>>>> master
+      },
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
